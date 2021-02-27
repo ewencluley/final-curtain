2b35a11c3b2f9712a3c2feef7dc823accd927327
Ewen Cluley <ewencluley@gmail.com>
2021-02-27 20:16:42 +0000

    add basic search form

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,9 @@
+import time
+
 from selenium import webdriver
 import unittest
+
+from selenium.webdriver.common.keys import Keys
 
 
 class HomepageTests(unittest.TestCase):
@@ -10,7 +14,12 @@ class HomepageTests(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def test_homepage_is_rendered(self):
+    def test_when_a_search_is_performed(self):
         self.browser.get('http://localhost:8000')
-        self.assertIn('Final Curtain', self.browser.title)
-        self.fail('Finish writing the test!')
+        search_field = self.browser.find_element_by_id('search')
+        search_field.send_keys("The Bill")
+        search_field.send_keys(Keys.ENTER)
+        time.sleep(1)
+        current_url: str = self.browser.current_url
+        self.assertRegex(current_url, r"^http://\w+:\d+/search.*$")
+
