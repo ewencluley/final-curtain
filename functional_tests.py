ae15474c701d19a2244690489ea46928ee39b342
Ewen Cluley <ewencluley@gmail.com>
2021-02-27 15:52:52 +0000

    add app with basic dummy homepage

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,16 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+class HomepageTests(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_homepage_is_rendered(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('Final Curtain', self.browser.title)
+        self.fail('Finish writing the test!')
