03929b8ee73ab7eccbf89911548fba57b831910c
Ewen Cluley <ewencluley@gmail.com>
2021-02-27 12:03:21 +0000

    Make alowed hosts configurable

--- finalcurtain/settings.py
+++ finalcurtain/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS','localhost,127.0.0.1').split(',')
 
 
 # Application definition
