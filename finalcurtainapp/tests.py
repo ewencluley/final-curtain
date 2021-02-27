ced09841790412d561d30efe2640918f1d1e0ca7
Ewen Cluley <ewencluley@gmail.com>
2021-02-27 17:51:06 +0000

    make homepage use template file

--- finalcurtainapp/tests.py
+++ finalcurtainapp/tests.py
@@ -1,18 +1,8 @@
-from django.http import HttpRequest
 from django.test import TestCase
-from django.urls import resolve
-from finalcurtainapp.views import home_page
+
 
 class HomepageTest(TestCase):
 
-    def test_homepage(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
-
     def test_homepage_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
-        response_body = response.content.decode('utf-8')
-        self.assertTrue(response_body.lower().startswith('<html>'))
-        self.assertIn('<title>Final Curtain</title>', response_body)
-        self.assertTrue(response_body.lower().endswith('</html>'))
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
